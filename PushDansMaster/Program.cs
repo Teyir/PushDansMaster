a5fb8550f456e6337af29ae266d52fba748517f9
Teyir <60007549+Teyir@users.noreply.github.com>
2021-11-15 08:42:45 +0100

    La console c b1

---
+++ PushDansMaster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PushDansMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
