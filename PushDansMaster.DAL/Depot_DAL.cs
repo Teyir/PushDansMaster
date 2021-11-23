87349842f64e0423df85bed03bee315a2f090a9e
Hodakai <gravy.thom@gmail.com>
2021-11-23 11:20:53 +0100

    Couche DAL et debut couche metier

---
+++ PushDansMaster.DAL/Depot_DAL.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace PushDansMaster.DAL
+{
+    //DAL class
+    abstract class Depot_DAL<Type_DAL> : IDepot_DAL<Type_DAL>
+    { 
+        public string connectionString { get; set; }
+
+        protected SqlConnection connection;
+        protected SqlCommand command;
+        
+        public Depot_DAL()
+        {
+            //Get the sqlserver configuration
+            var builder = new ConfigurationBuilder();
+            var config = builder.AddJsonFile("appsettings.json", true, true).Build();
+
+            connectionString = config.GetSection("ConnectionStrings:defailt").Value;
+        }
+
+
+        //Create the connection to the sql server
+        protected void createConnection()
+        {
+            connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            command = new SqlCommand();
+            command.Connection = connection;
+
+            connection.Close();
+        }
+
+        //Close the connection to the sql server
+        protected void closeConnection()
+        {
+            command.Dispose();
+            connection.Close();
+            command.Dispose();
+        }
+
+        #region Abstract methods
+        public abstract void delete(Type_DAL item);
+
+        public abstract List<Type_DAL> getAll();
+
+        public abstract Type_DAL getByID(int ID);
+
+        public abstract Type_DAL insert(Type_DAL item);
+
+        public abstract Type_DAL update(Type_DAL item);
+        #endregion
+    }
+}
+
---
+++ PushDansMaster.DAL/FournisseurDepot_DAL.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushDansMaster.DAL
+{
+    class FournisseurDepot_DAL : Depot_DAL<Fournisseur_DAL>
+    {
+
+        public override List<Fournisseur_DAL> getAll()
+        {
+            createConnection();
+
+            command.CommandText = "SELECT id,  societe, civilite, nom, prenom, email, adresse FROM fournisseur";
+            var reader = command.ExecuteReader();
+
+            var listFournisseur = new List<Fournisseur_DAL>();
+
+            while (reader.Read())
+            {
+                var f = new Fournisseur_DAL(reader.GetInt32(0),
+                                                reader.GetString(1),
+                                                reader.GetBoolean(2),
+                                                reader.GetString(3),
+                                                reader.GetString(4),
+                                                reader.GetString(5),
+                                                reader.GetString(6));
+                listFournisseur.Add(f);
+            }
+
+            closeConnection();
+
+            return listFournisseur;
+        }
+
+
+        public override Fournisseur_DAL getByID(int ID)
+        {
+            createConnection();
+
+            command.CommandText = "select id,  societe, civilite, nom, prenom, email, adresse FROM fournisseur WHERE id=@id";
+            command.Parameters.Add(new SqlParameter("@id", ID));
+            var reader = command.ExecuteReader();
+
+            var listFournisseur = new List<Fournisseur_DAL>();
+
+            Fournisseur_DAL f;
+            if (reader.Read())
+            {
+                f = new Fournisseur_DAL(reader.GetInt32(0),
+                                                reader.GetString(1),
+                                                reader.GetBoolean(2),
+                                                reader.GetString(3),
+                                                reader.GetString(4),
+                                                reader.GetString(5),
+                                                reader.GetString(6));
+            }
+            else
+                throw new Exception($"Pas de fournisseur avec l'ID {ID}");
+
+             closeConnection();
+
+            return f;
+        }
+
+
+        public override Fournisseur_DAL insert(Fournisseur_DAL fournisseur)
+        {
+            createConnection();
+
+            command.CommandText = "INSERT INTO fournisseur(societe, civilite, nom, prenom, email, adresse)"
+                                    + " VALUES (@societe, @civilite, @nom, @prenom, @email, @adresse); select scope_identity()";
+
+            command.Parameters.Add(new SqlParameter("@societe", fournisseur.societeFournisseur));
+            command.Parameters.Add(new SqlParameter("@civilite", fournisseur.civiliteFournisseur));
+            command.Parameters.Add(new SqlParameter("@nom", fournisseur.nomFournisseur));
+            command.Parameters.Add(new SqlParameter("@prenom", fournisseur.prenomFournisseur));
+            command.Parameters.Add(new SqlParameter("@email", fournisseur.emailFournisseur));
+            command.Parameters.Add(new SqlParameter("@adresse", fournisseur.adresseFournisseur));
+
+            var ID = Convert.ToInt32((decimal)command.ExecuteScalar());
+
+            fournisseur.idFournisseur = ID;
+
+            closeConnection();
+
+            return fournisseur;
+
+        }
+
+        public override Fournisseur_DAL update(Fournisseur_DAL fournisseur)
+        {
+            createConnection();
+
+            command.CommandText = "UPDATE fournisseur set societe=@societe, civilite=@civilite, nom=@nom,"
+                                    +  " prenom=@prenom, email=@email, adresse=@adresse"
+                                    +  " WHERE id=@ID";
+
+            command.Parameters.Add(new SqlParameter("@ID", fournisseur.idFournisseur));
+            command.Parameters.Add(new SqlParameter("@societe", fournisseur.societeFournisseur));
+            command.Parameters.Add(new SqlParameter("@civilite", fournisseur.civiliteFournisseur));
+            command.Parameters.Add(new SqlParameter("@nom", fournisseur.nomFournisseur));
+            command.Parameters.Add(new SqlParameter("@prenom", fournisseur.prenomFournisseur));
+            command.Parameters.Add(new SqlParameter("@email", fournisseur.emailFournisseur));
+            command.Parameters.Add(new SqlParameter("@adresse", fournisseur.adresseFournisseur));
+
+            var linesAffected = (int)command.ExecuteNonQuery();
+
+            if(linesAffected != 1)
+            {
+                throw new Exception($"Impossible de mettre à jour le fournisseur {fournisseur.idFournisseur}");
+            }
+
+            closeConnection();
+
+            return fournisseur;
+
+        }
+
+
+
+        public override void delete(Fournisseur_DAL fournisseur)
+        {
+            createConnection();
+
+            command.CommandText = "DELETE FROM fournisseur WHERE id=@ID";
+            command.Parameters.Add(new SqlParameter("@ID", fournisseur.idFournisseur));
+
+            var linesAffected = (int)command.ExecuteNonQuery();
+
+            if (linesAffected != 1)
+            {
+                throw new Exception($"Impossible de supprimer le fournisseur {fournisseur.idFournisseur}");
+            }
+
+            closeConnection();
+
+        }
+
+
+    }
+}
