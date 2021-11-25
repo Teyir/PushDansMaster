b34b865c21fb92115b8cb447261caaacf761e11c
Hodakai <gravy.thom@gmail.com>
2021-11-25 11:17:01 +0100

    Fix panier global + ajout du panier adherent

--- PushDansMaster.DAL/PanierAdherent_DAL.cs
+++ PushDansMaster.DAL/PanierAdherent_DAL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,27 @@ namespace PushDansMaster.DAL
 {
     class PanierAdherent_DAL
     {
+        public int ID { get; }
+        public bool status { get; private set; }
+        public int semaine { get; private set; }
+        public int id_adherent { get; private set; }
+        public int id_panierGlobal { get; private set; }
+        public PanierAdherent_DAL(bool status, int semaine, int id_adh, int id_panierGlo) => (this.status, this.semaine, this.id_adherent, this.id_panierGlobal) = (status, semaine, id_adh, id_panierGlo);
+
+        internal void insert(SqlConnection connection)
+        {
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO panier_adherent(status, semaine, id_adherent, id_panierglobal) values (@status, @semaine, @id_adh, @id_panierGlo)";
+
+                command.Parameters.Add(new SqlParameter("@status", status));
+                command.Parameters.Add(new SqlParameter("@semaine", semaine));
+                command.Parameters.Add(new SqlParameter("@id_adh", id_adherent));
+                command.Parameters.Add(new SqlParameter("@id_panierGlo", id_panierGlobal));
+
+                command.ExecuteNonQuery();
+            }
+        }
     }
 }
--- PushDansMaster/PanierAdherent.cs
+++ PushDansMaster/PanierAdherent.cs
@@ -8,6 +8,58 @@ namespace PushDansMaster
 {
     class PanierAdherent
     {
-        
+        private int ID;
+        private bool status;
+        private int semaine;
+        private int id_adherent;
+        private int id_panierGlobal;
+
+        #region Getters / Setters
+
+        /// <summary>
+        /// Permet d'obtenir le status
+        /// </summary>
+        public bool getStatus
+        {
+            get { return status; }
+            private set { status = value; }
+        }
+
+        public int getSemaine
+        {
+            get { return semaine; }
+            private set { semaine = value; }
+        }
+
+        public int getID
+        {
+            get { return ID; }
+            private set { ID = value; }
+        }
+
+        public int getID_adherent
+        {
+            get { return id_adherent; }
+            private set { id_adherent = value; }
+        }
+
+        public int getID_panierGlobal
+        {
+            get { return id_panierGlobal; }
+            private set { id_panierGlobal = value; }
+        }
+        #endregion
+
+        #region Constructeurs
+
+        public PanierAdherent(bool status, int semaine, int id_adherent, int id_panierGlobal)
+        {
+            this.status = status;
+            this.semaine = semaine;
+            this.id_adherent = id_adherent;
+            this.id_panierGlobal = id_panierGlobal;
+        }
+
+        #endregion
     }
 }
