649a297f2b9ae661e7c441bb9167f80518328735
Teyir <60007549+Teyir@users.noreply.github.com>
2021-11-25 09:04:24 +0100

    [IMPROVE] Clean & improve

--- PushDansMaster.DAL/PanierGlobal_DAL.cs
+++ PushDansMaster.DAL/PanierGlobal_DAL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,23 @@ namespace PushDansMaster.DAL
 {
     class PanierGlobal_DAL
     {
+        public int ID { get; }
+        public bool status { get; private set; }
+        public int semaine { get; private set; }
+        public PanierGlobal_DAL(bool status, int semaine) => (this.status, this.semaine) = (status, semaine);
 
+        internal void insert(SqlConnection connection)
+        {
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO panier_global(status, semaine) values (@status, @semaine)";
+
+                command.Parameters.Add(new SqlParameter("@status", status));
+                command.Parameters.Add(new SqlParameter("@semaine", semaine));
+
+                command.ExecuteNonQuery();
+            }
+        }
     }
 }
--- PushDansMaster/FournisseurService.cs
+++ PushDansMaster/FournisseurService.cs
@@ -1,4 +1,4 @@
-﻿using PushDansMaster.DAL
+﻿using PushDansMaster.DAL;
 using System.Collections.Generic;
 using System.Linq;
 
