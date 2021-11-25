50ad055b0d012f5cbad956340f030f5897f70536
Axel BAUDOIN <axel.baudoin@epsi.fr>
2021-11-25 10:53:18 +0100

    Interface Adhérent + Service Adhérent

--- PushDansMaster/AdherentService.cs
+++ PushDansMaster/AdherentService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PushDansMaster.DAL;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,49 @@ using System.Threading.Tasks;
 
 namespace PushDansMaster
 {
-    public class AdherentService
+    public class AdherentService : IAdherentService
     {
+        private AdherentDepot_DAL depot = new AdherentDepot_DAL();
+
+        public List<Adherent> getAll()
+        {
+            var adherents = depot.getAll()
+                .Select(f => new Adherent(f.societeAdherent, f.emailAdherent, f.nomAdherent, f.prenomAdherent, f.adresseAdherent, f.dateAdhesionAdherent,f.statusAdherent))
+                .ToList();
+            return adherents;
+        }
+
+        public Adherent getByID(int ID)
+        {
+            var f = depot.getByID(ID);
+
+
+            return new Adherent(f.societeAdherent, f.emailAdherent, f.nomAdherent, f.prenomAdherent, f.adresseAdherent, f.dateAdhesionAdherent, f.statusAdherent);
+
+        }
+
+        public Adherent insert(Adherent f)
+        {
+            var adherent = new Adherent_DAL(f.idAdherent, f.societeAdherent, f.emailAdherent, f.nomAdherent, f.prenomAdherent, f.adresseAdherent, f.dateAdhesionAdherent, f.statusAdherent);
+            depot.insert(adherent);
+
+
+            return f;
+        }
+
+        public Adherent update(Adherent f)
+        {
+            var adherent = new Adherent_DAL(f.idAdherent, f.societeAdherent, f.emailAdherent, f.nomAdherent, f.prenomAdherent, f.adresseAdherent, f.dateAdhesionAdherent, f.statusAdherent);
+            depot.update(adherent);
+
+            return f;
+        }
+
+        public void delete(Adherent f)
+        {
+            var adherent = new Adherent_DAL(f.idAdherent, f.societeAdherent, f.emailAdherent, f.nomAdherent, f.prenomAdherent, f.adresseAdherent, f.dateAdhesionAdherent, f.statusAdherent);
+            depot.delete(adherent);
+        }
+
     }
 }
--- PushDansMaster/IAdherentService.cs
+++ PushDansMaster/IAdherentService.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PushDansMaster
 {
-    interface IAdherentService
+    public interface IAdherentService
     {
+        public List<Adherent> getAll();
+
+        public Adherent getByID(int ID);
+
+        public Adherent insert(Adherent f);
+
+        public Adherent update(Adherent f);
+
+        public void delete(Adherent f);
     }
 }
