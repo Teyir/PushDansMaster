34d860eee793d6cce862fb3eb4c3d1a5285be5b8
Axebita1 <axel.baudoin@epsi.fr>
2021-11-30 00:16:22 +0100

    Fix PanierGlobal : Getters/setters + SQL commands and Errors

--- PushDansMaster.DAL/PanierGlobal_DAL.cs
+++ PushDansMaster.DAL/PanierGlobal_DAL.cs
@@ -1,25 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
 
 namespace PushDansMaster.DAL
 {
-    class PanierGlobal_DAL
+    public class PanierGlobal_DAL
     {
-        public int ID { get; }
-        public bool status { get; private set; }
-        public int semaine { get; private set; }
-        public PanierGlobal_DAL(bool status, int semaine) => (this.status, this.semaine) = (status, semaine);
+        private int ID;
+        private bool status;
+        private int semaine;
 
+        #region Getters / Setters
+        public int getID
+        {
+            get { return ID; }
+            private set { ID = value; }
+        }
+        public int getSemaine
+        {
+            get { return semaine; }
+            private set { semaine = value; }
+        }
+        public bool getStatus
+        {
+            get { return status; }
+            private set { status = value; }
+        }
+
+        #endregion
+
+        #region Constructeurs
+        public PanierGlobal_DAL(bool status, int semaine) => (this.status, this.semaine) = (status, semaine);
+        public PanierGlobal_DAL(int id, bool status, int semaine) => (this.ID, this.status, this.semaine) = (id, status, semaine);
+        #endregion
+
+        #region Methodes
         internal void insert(SqlConnection connection)
         {
             using (var command = new SqlCommand())
             {
                 command.Connection = connection;
-                command.CommandText = "INSERT INTO panier_global(status, semaine) values (@status, @semaine)";
+                command.CommandText = "INSERT INTO panier_global(status, semaine) VALUES (@status, @semaine)";
 
                 command.Parameters.Add(new SqlParameter("@status", status));
                 command.Parameters.Add(new SqlParameter("@semaine", semaine));
@@ -27,5 +46,6 @@ namespace PushDansMaster.DAL
                 command.ExecuteNonQuery();
             }
         }
+        #endregion 
     }
 }
