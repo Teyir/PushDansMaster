114c7829888a35f6f4a2f5a513d632b329e92a65
Hodakai <gravy.thom@gmail.com>
2021-12-02 10:06:48 +0100

    [INIT] Tests DAL

--- PushDansMaster/Program.cs
+++ PushDansMaster/Program.cs
@@ -11,34 +11,6 @@ namespace PushDansMaster
             Console.WriteLine("Push dans master...");
 
 
-            /* TEST AJOUT D'UN FOURNISSEUR
-
-            Console.WriteLine("Merci d'entrer le nom de la societe");
-            string societe = Console.ReadLine();
-
-           // Console.WriteLine("Merci d'entrer votre civilite");
-            bool civilite = true;
-
-
-            Console.WriteLine("Merci d'entrer votre nom");
-            string nom = Console.ReadLine();
-
-            Console.WriteLine("Merci d'entrer votre prenom");
-            string prenom = Console.ReadLine();
-
-            Console.WriteLine("Merci d'entrer votre email");
-            string email = Console.ReadLine();
-
-            Console.WriteLine("Merci d'entrer votre adresse");
-            string adresse =  Console.ReadLine();
-
-            var ezez = new Fournisseur(societe, civilite, nom, prenom, email, adresse);
-
-            Console.WriteLine(ezez.societeFournisseur, ezez.civiliteFournisseur, ezez.nomFournisseur, ezez.prenomFournisseur, ezez.emailFournisseur, ezez.adresseFournisseur);
-        
-            */
-
-            ///Import fournisseur
 
             var import = new importReferences();
 
