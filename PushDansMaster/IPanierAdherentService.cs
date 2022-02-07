97a220f1a692f083f741fa263c2d0b034b9aec03
Teyir <60007549+Teyir@users.noreply.github.com>
2022-02-07 19:30:27 +0100

    [IMPROVE] Add panier adherent API

---
+++ PushDansMaster/IPanierAdherentService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushDansMaster
+{
+    public interface IPanierAdherentService
+    {
+        public List<PanierAdherent> getAll();
+
+        public PanierAdherent getByID(int ID);
+
+        public PanierAdherent insert(PanierAdherent f);
+
+    }
+}
